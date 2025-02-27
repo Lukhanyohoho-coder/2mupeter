a28e501a91829b84880625875f9c3197c29ad80d
Administrator <Lukhanyohoho@outlook.com>
2025-02-27 13:41:55 +0000

    Merge remote-tracking branch 'origin/main' into main

--- src/main/java/runners/TestRunner.java
+++ src/main/java/runners/TestRunner.java
@@ -6,6 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/main/resources/features/",
         glue = "stepDefinitions",
+
         plugin = {"pretty", "html:target/cucumber-reports.html",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         publish = true
